d5611ee788787d82790c87de3e75cf9a8226a768
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2026-02-14 18:19:29 +0530

    React Boiler plate

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import TaskInput from './components/TaskInput';
-import { useState } from 'react';
-import TaskList from './components/TaskList';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const addTask = (newTask) => {
-    setTasks((prev) => [...prev, newTask]);
-  }
-
-  const removeTask = (indexToRemove) => {
-    setTasks((prevTasks) => {
-      return prevTasks.filter((_, index) => index !== indexToRemove)
-    });
-  }
-
   return (
     <div className="App">
-      <Header/>
-      <div className='task_wrapper'>
-        <div className='task_box'>
-          <TaskInput onAddTask={addTask}/>
-          <TaskList tasks={tasks} removeTasks={removeTask}/>
-        </div>
-      </div>
+      Lets Start from scratch
     </div>
   );
 }
