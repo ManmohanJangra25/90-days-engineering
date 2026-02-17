8f36556ade1405d790460db56e8bbec3fb554198
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2026-02-18 00:19:58 +0530

    Local Storage Updates

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-import './App.css';
-import Header from './components/Header';
-import TaskInput from './components/TaskInput';
-import { useState } from 'react';
-import TaskList from './components/TaskList';
+import "./App.css";
+import Header from "./components/Header";
+import TaskInput from "./components/TaskInput";
+import { useEffect, useState } from "react";
+import TaskList from "./components/TaskList";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const addTask = (newTask) => {
-    setTasks((prev) => [...prev, {
-      id: Math.random(),
-      text: newTask,
-      isCompleted: false,
-      createdAt: Date.now()
-    }]);
-  }
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (savedTasks) {
+      setTasks(savedTasks);
+    }
+  }, []);
 
-  const removeTask = (indexToRemove) => {
+  const addTask = (newTask) => {
+    setTasks((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        text: newTask,
+        isCompleted: false,
+        createdAt: Date.now(),
+      },
+    ]);
+  };
+
+  const removeTask = (idToRemove) => {
     setTasks((prevTasks) => {
-      return prevTasks.filter((task, index) => task.id !== indexToRemove);
+      return prevTasks.filter((task, index) => task.id !== idToRemove);
     });
   };
 
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
     <div className="App">
-      <Header/>
-      <div className='task_wrapper'>
-        <div className='task_box'>
-          <TaskInput onAddTask={addTask}/>
-          <TaskList tasks={tasks} removeTasks={removeTask}/>
+      <Header />
+      <div className="task_wrapper">
+        <div className="task_box">
+          <TaskInput onAddTask={addTask} />
+          <TaskList tasks={tasks} removeTasks={removeTask} />
         </div>
       </div>
     </div>
